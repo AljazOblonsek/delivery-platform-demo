e10b5026914684beae434ae2999dc601cabfe186
AljazOblonsek <42306093+AljazOblonsek@users.noreply.github.com>
2024-09-18 21:47:37 +0200

    Connect Profile Page with Information from Auth Store (#6)
    * test: add unit test for user information on profile page
    
    * feat: display user informtation on profile page
    
    * test: add unit test for logout button click
    
    * feat: implement logout functionality
    
    * chore: reduce spacing between inputs and logout button
    
    * test: add assertion jwt removal from local storage
    
    * feat: remove jwt from local storage on logout

---
+++ services/delivery-ui/src/features/profile/pages/ProfilePage.spec.tsx
@@ -0,0 +1,60 @@
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { ProfilePage } from './ProfilePage';
+import { LocalStorageKey, User } from '@/core';
+
+const userStub: User = {
+  id: '1',
+  email: 'john.doe@dp-demo.io',
+  firstname: 'John',
+  lastname: 'Doe',
+  companyId: '1',
+  companyName: 'Beeliver',
+};
+
+const unauthenticateSpy = vi.fn();
+
+vi.mock('@/core', async (importOriginal) => {
+  const original: object = await importOriginal();
+
+  return {
+    ...original,
+    useAuthStore: () => ({
+      user: userStub,
+      unauthenticate: unauthenticateSpy,
+    }),
+  };
+});
+
+describe('ProfilePage', () => {
+  afterEach(() => {
+    cleanup();
+
+    vi.clearAllMocks();
+  });
+
+  it('should render user information from the auth store on form', async () => {
+    render(<ProfilePage />);
+
+    expect(await screen.findByTestId('email-input')).toHaveValue(userStub.email);
+    expect(await screen.findByTestId('firstname-input')).toHaveValue(userStub.firstname);
+    expect(await screen.findByTestId('lastname-input')).toHaveValue(userStub.lastname);
+    expect(await screen.findByTestId('delivery-company-input')).toHaveValue(userStub.companyName);
+  });
+
+  it('should unauthenticate user on logout button click', async () => {
+    const removeItemSpy = vi.spyOn(Storage.prototype, 'removeItem');
+
+    render(<ProfilePage />);
+
+    const logoutButton = screen.getByTestId('logout-button');
+
+    fireEvent.click(logoutButton);
+
+    await waitFor(() => {
+      expect(removeItemSpy).toHaveBeenCalledTimes(1);
+      expect(removeItemSpy).toHaveBeenCalledWith(LocalStorageKey.AccessToken);
+      expect(unauthenticateSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+});
--- services/delivery-ui/src/features/profile/pages/ProfilePage.tsx
+++ services/delivery-ui/src/features/profile/pages/ProfilePage.tsx
@@ -1,41 +1,64 @@
 import { Button, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
-import { useLocation } from 'wouter';
-import { DashboardLayout, Routes } from '@/core';
+import { DashboardLayout, LocalStorageKey, useAuthStore } from '@/core';
 
 export const ProfilePage = () => {
-  const [_, setLocation] = useLocation();
+  const { user, unauthenticate } = useAuthStore((state) => ({
+    user: state.user,
+    unauthenticate: state.unauthenticate,
+  }));
 
   return (
     <DashboardLayout title="Profile" description="Information about your profile.">
-      <Stack spacing={20} as="form">
+      <Stack spacing={10} as="form">
         <Stack spacing={5} fontSize="lg">
           <FormControl>
             <FormLabel htmlFor="email">Email</FormLabel>
-            <Input id="email" variant="filled" disabled={true} value="john.doe@gmail.com" />
+            <Input
+              id="email"
+              data-testid="email-input"
+              variant="filled"
+              disabled={true}
+              value={user?.email}
+            />
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="firstname">Firstname</FormLabel>
-            <Input id="firstname" variant="filled" disabled={true} value="John" />
+            <Input
+              id="firstname"
+              data-testid="firstname-input"
+              variant="filled"
+              disabled={true}
+              value={user?.firstname}
+            />
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="lastname">Lastname</FormLabel>
-            <Input id="firstname" variant="filled" disabled={true} value="Doe" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="deliverycompany">Delivery company</FormLabel>
             <Input
-              id="deliverycompany"
+              id="firstname"
+              data-testid="lastname-input"
               variant="filled"
               disabled={true}
-              value="Beeliver Delivers"
+              value={user?.lastname}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="delivery-company">Delivery company</FormLabel>
+            <Input
+              id="delivery-company"
+              data-testid="delivery-company-input"
+              variant="filled"
+              disabled={true}
+              value={user?.companyName}
             />
           </FormControl>
         </Stack>
 
         <Button
           onClick={() => {
-            setLocation(Routes.Login);
+            localStorage.removeItem(LocalStorageKey.AccessToken);
+            unauthenticate();
           }}
+          data-testid="logout-button"
         >
           Logout
         </Button>
